3c3050789ccc75283104b4307c3bbaebce815750
cvillarr <c.villarr@alumnos.urjc.es>
2018-10-16 14:37:08 +0200

    programa terminado y funcionando

--- karaoke.py
+++ karaoke.py
@@ -13,9 +13,9 @@ from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 from urllib.request import urlretrieve
 
+
 class KaraokeLocal():
-    
-    
+
     def __init__(self, fich):
         parser = make_parser()
         cHandler = SmallSMILHandler()
@@ -34,47 +34,32 @@ class KaraokeLocal():
                 listattr.append(attr + "=" + '"' + atributos[attr] + '"')
             listattr = "\t".join(listattr)
             archivosmil.append(listattr + "\n")
-        print (archivosmil)
-            
-    def to_json(self,fichjson):
-        with open (fichjson, "w"):
+        print(archivosmil)
+
+    def to_json(self, fichjson):
+        with open(fichjson, "w"):
             json.dumps(self.listval)
-        
+
     def do_local(self):
         for linea in self.listval:
             atributos = linea[1]
-            for attr,valor in atributos.items():
+            for attr, valor in atributos.items():
                 if attr == "src" and valor[:7] == "http://":
                     nombre = valor.split("/")[-1]
                     urlretrieve(valor, nombre)
-                    print ("Descargando %s ..." % valor)
+                    print("Descargando %s ..." % valor)
 
-                    
-                    
+
 if __name__ == "__main__":
     try:
         fichsmil = sys.argv[1]
-        fichjson = sys.argv[1].replace(".smil",".json")
+        fichjson = sys.argv[1].replace(".smil", ".json")
         fichkaraokeloc = KaraokeLocal(fichsmil)
         fichkaraokeloc.__str__()
         fichkaraokeloc.to_json(fichjson)
         fichkaraokeloc.do_local()
         fichkaraokeloc.to_json('local.json')
         fichkaraokeloc.__str__()
-        
-
 
     except IndexError:
         sys.exit("Usage:python3 karaoke.py file.smil")
-
-
-
-
-
-
-
-
-
-
-
-
