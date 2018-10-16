47c782b30f35d5867276919e1f33f4f624cc4f3b
cvillarr <c.villarr@alumnos.urjc.es>
2018-10-16 13:16:52 +0200

    karaoke funcionando con json

--- karaoke.py
+++ karaoke.py
@@ -6,6 +6,7 @@ Created on Mon Oct 15 11:02:00 2018
 @author: cvillarr
 """
 import sys
+import json
 
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
@@ -13,28 +14,36 @@ from smallsmilhandler import SmallSMILHandler
 
 try:
     fichsmil = sys.argv[1]
+    fichjson = sys.argv[1].replace(".smil",".json")
+    archivosmil = []
+
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(fichsmil))
     listval = cHandler.get_tags()
 
-    for linea in listval:
-        listatotal = []
-        listattr = []
-        etiq = linea[0]
-        atributos = linea[1]
-        listattr.append(etiq)
-        for attr in atributos:
-            listattr.append(attr + "=" + '"' + atributos[attr] + '"')
-        listattr = "\\".join(listattr)
+    with open (fichjson, "w") as fichero_json:
+    
 
-        print (listattr)
+        for linea in listval:
+            listatotal = []
+            listattr = []
+            etiq = linea[0]
+            atributos = linea[1]
+            listattr.append(etiq)
+            for attr in atributos:
+                listattr.append(attr + "=" + '"' + atributos[attr] + '"')
+            listattr = "\t".join(listattr)
+            archivosmil.append(listattr + "\n")
+        fichero_json = json.dumps(archivosmil)
+    
+    print (fichero_json)
 
 
 except IndexError:
     sys.exit("Usage:python3 karaoke.py file.smil")
-    
+
 
 
 
