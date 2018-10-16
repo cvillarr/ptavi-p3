e9d4e1ca9f3af876ead51fa4f2bc58f94b737f4f
cvillarr <c.villarr@alumnos.urjc.es>
2018-10-16 13:37:17 +0200

    programa descarga archivos

--- karaoke.py
+++ karaoke.py
@@ -8,9 +8,10 @@ Created on Mon Oct 15 11:02:00 2018
 import sys
 import json
 
+
 from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
-
+from urllib.request import urlretrieve
 
 try:
     fichsmil = sys.argv[1]
@@ -25,7 +26,7 @@ try:
 
     with open (fichjson, "w") as fichero_json:
     
-
+        #creando archivo
         for linea in listval:
             listatotal = []
             listattr = []
@@ -36,10 +37,18 @@ try:
                 listattr.append(attr + "=" + '"' + atributos[attr] + '"')
             listattr = "\t".join(listattr)
             archivosmil.append(listattr + "\n")
-        fichero_json = json.dumps(archivosmil)
-    
-    print (fichero_json)
+        
+        """#json
+        fichero_json = json.dumps(archivosmil)"""
 
+        for linea in listval:
+            atributos = linea[1]
+            for attr,valor in atributos.items():
+                if attr == "src" and valor[:7] == "http://":
+                    nombre = valor.split("/")[-1]
+                    urlretrieve(valor, nombre)
+                    print ("Descargando %s ..." % valor)
+                    print (nombre)
 
 except IndexError:
     sys.exit("Usage:python3 karaoke.py file.smil")
