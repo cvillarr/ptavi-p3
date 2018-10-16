16476fcdb6a45053022a27463dd02692f08bf56f
cvillarr <c.villarr@alumnos.urjc.es>
2018-10-16 14:33:36 +0200

    programa terminado sin pep8

--- karaoke.py
+++ karaoke.py
@@ -13,22 +13,19 @@ from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 from urllib.request import urlretrieve
 
-try:
-    fichsmil = sys.argv[1]
-    fichjson = sys.argv[1].replace(".smil",".json")
-    archivosmil = []
-
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(fichsmil))
-    listval = cHandler.get_tags()
-
-    with open (fichjson, "w") as fichero_json:
+class KaraokeLocal():
     
-        #creando archivo
-        for linea in listval:
-            listatotal = []
+    
+    def __init__(self, fich):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichsmil))
+        self.listval = cHandler.get_tags()
+
+    def __str__(self):
+        archivosmil = []
+        for linea in self.listval:
             listattr = []
             etiq = linea[0]
             atributos = linea[1]
@@ -37,21 +34,38 @@ try:
                 listattr.append(attr + "=" + '"' + atributos[attr] + '"')
             listattr = "\t".join(listattr)
             archivosmil.append(listattr + "\n")
+        print (archivosmil)
+            
+    def to_json(self,fichjson):
+        with open (fichjson, "w"):
+            json.dumps(self.listval)
         
-        """#json
-        fichero_json = json.dumps(archivosmil)"""
-
-        for linea in listval:
+    def do_local(self):
+        for linea in self.listval:
             atributos = linea[1]
             for attr,valor in atributos.items():
                 if attr == "src" and valor[:7] == "http://":
                     nombre = valor.split("/")[-1]
                     urlretrieve(valor, nombre)
                     print ("Descargando %s ..." % valor)
-                    print (nombre)
 
-except IndexError:
-    sys.exit("Usage:python3 karaoke.py file.smil")
+                    
+                    
+if __name__ == "__main__":
+    try:
+        fichsmil = sys.argv[1]
+        fichjson = sys.argv[1].replace(".smil",".json")
+        fichkaraokeloc = KaraokeLocal(fichsmil)
+        fichkaraokeloc.__str__()
+        fichkaraokeloc.to_json(fichjson)
+        fichkaraokeloc.do_local()
+        fichkaraokeloc.to_json('local.json')
+        fichkaraokeloc.__str__()
+        
+
+
+    except IndexError:
+        sys.exit("Usage:python3 karaoke.py file.smil")
 
 
 
